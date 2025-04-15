2863f57741e95a41a0c485cde12a41e417347c80
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-04-15 12:21:48 +0500

    changed title

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st 
 import pickle
-
-st.title("Salary Predictor")
-
+st.title("Sagar Predictor")
 with open("model.pkl", "rb") as file:
     model = pickle.load(file)
-
 user_input = st.number_input("Experience Years", min_value=0.0, max_value=50.0, value=0.0, step=0.1)
-
 st.success(model.predict( [[user_input]])[0] )
