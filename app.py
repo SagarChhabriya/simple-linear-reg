c2f4d350166b06e727d84dce6fd62e66f187a3ec
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-04-15 12:25:28 +0500

    add button

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st 
 import pickle
+
 st.title("Sagar Predictor")
+
 with open("model.pkl", "rb") as file:
     model = pickle.load(file)
+
 user_input = st.number_input("Experience Years", min_value=0.0, max_value=50.0, value=0.0, step=0.1)
-st.success(model.predict( [[user_input]])[0] )
+
+if st.button:
+    st.success(model.predict( [[user_input]])[0] )
